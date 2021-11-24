ed08e4809f5a532b19cb0a6ff5b619091b6bca78
Nayeon Kim <inourbubble2@gmail.com>
2021-11-24 18:37:58 +0900

    feat: 카페 등록 시 Kakao API에서 정보를 가져온 후 삽입을 한다

--- api/dto/cafe_dto.py
+++ api/dto/cafe_dto.py
@@ -47,7 +47,7 @@ paginated_cafe = api.model('PaiginatedCafe', {
 cafe_create_request = api.model('CafeCreateRequest', {
     'name': fields.String(required=True, example="964커피&너만보여크루아상 회기점"),
     'description': fields.String(example="#로스팅전문 #베이커리카페"),
-    'address': fields.String(example="서울 동대문구 망우로21길 63 (우)02439"),
+    'address': fields.String(required=True, example="서울 동대문구 망우로21길 63 (우)02439"),
     'open_hour': fields.String(example="매일 08:00 ~ 22:00"),
     'contact': fields.String(example="02-1234-5678"),
     'link': fields.String(example="www.instagram.com/964__coffee"),
--- api/services/cafe_service.py
+++ api/services/cafe_service.py
@@ -1,3 +1,5 @@
+import requests
+
 from werkzeug.exceptions import BadRequest
 
 from api.services.service import Service
@@ -15,6 +17,18 @@ class CafeService(Service):
                 raise BadRequest('star is not valid range.')
             elif key == 'location' and not value in {'hoegi', 'front', 'side', 'back'}:
                 raise BadRequest('location is not valid.')
+
+        header = {'Authorization': 'KakaoAK e739d059fb281ac73a2ca928910f688d'}
+        query = data['address'] + ' ' + data['name']
+        url = f'https://dapi.kakao.com/v2/local/search/keyword.json?sort=accuracy&query={query}&category_group_code=CE7&size=1'
+        response = requests.get(url, headers=header).json()
+
+        data['name'] = response['documents'][0]['place_name']
+        data['longitude'] = response['documents'][0]['x']
+        data['latitude'] = response['documents'][0]['y']
+        data['address'] = response['documents'][0]['road_address_name']
+        data['kakao_id'] = response['documents'][0]['id']
+
         super().create(data)
 
     def find_with_pagination_and_arguments(self, page, per_page, name, location):
