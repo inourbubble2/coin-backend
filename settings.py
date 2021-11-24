8ea769a6873050d5416d140da60b6e2e41b3b3f3
Nayeon Kim <inourbubble2@gmail.com>
2021-11-24 19:34:12 +0900

    feat: db uri setting 을 위한 env variable 추가

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+import os
+
 # Flask settings
 FLASK_SERVER_NAME = '0.0.0.0:5000'
 FLASK_DEBUG = True  # Do not use debug mode in production
@@ -9,5 +11,9 @@ RESTPLUS_MASK_SWAGGER = False
 RESTPLUS_ERROR_404_HELP = False
 
 # SQLAlchemy settings
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@coin-database:5432/coin-database'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+if 'MODE' in os.environ and os.environ['MODE'] == 'docker':
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@coin-database:5432/coin-database'
+else:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/coin-database'
+
